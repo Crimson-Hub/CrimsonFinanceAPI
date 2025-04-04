563a36f021619a8a06a2708f195f062c051b2256
yuri9 <yurilsilva93@gmail.com>
2025-04-04 15:26:39 -0300

    feat(database): add current_expenses column to card table
    Added the 'current_expenses' column to the 'card' table to track the current amount of expenses per card.
    The column is of type DECIMAL(12, 2) with a default value of 0.00 and is NOT NULL.

--- src/main/resources/db/migration/V3__card-table.sql
+++ src/main/resources/db/migration/V3__card-table.sql
@@ -3,5 +3,6 @@ CREATE TABLE card(
     id_profile BIGINT NOT NULL,
     expiry_day INTEGER NOT NULL,
     credit_limit DECIMAL(12, 2) NOT NULL DEFAULT 0.00,
+    current_expenses DECIMAL(12, 2) NOT NULL 0.00,
     CONSTRAINT fk_profile FOREIGN KEY(id_profile) REFERENCES profile(id) ON DELETE CASCADE
 );
